6d660523de3e5f9c2eea917d091a896218ae3fb1
anhduc2204 <101176957+anhduc2204@users.noreply.github.com>
2024-07-04 19:49:27 +0700

    add all

---
+++ QLTuyenDung/Models/DataContext.cs
@@ -0,0 +1,33 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+
+namespace QLTuyenDung.Models
+{
+    public class DataContext : DbContext
+    {
+
+        protected readonly IConfiguration Configuration;
+
+        public DataContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public DbSet<NguoiDung> DSNguoiDung { get; set; }
+        public DbSet<TaiKhoan> DSTaiKhoan { get; set; }
+
+        public DbSet<QuyenHan> DSQuyen {  get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            ILoggerFactory loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
+
+            optionsBuilder.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+                .UseLoggerFactory(loggerFactory);
+        }
+    }
+}
