32f66257baf05da0860109c659676293e5542096
anhduc2204 <101176957+anhduc2204@users.noreply.github.com>
2024-07-06 14:41:07 +0700

    Tin tuyen dung

---
+++ QLTuyenDung/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QLTuyenDung.DAO;
+
+namespace QLTuyenDung.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ViecLamDAO _ViecLamdao;
+
+        public AdminController(ViecLamDAO viecLamDAO)
+        {
+            _ViecLamdao = viecLamDAO;
+        }
+
+        [HttpGet]
+        [Route("Admin/")]
+        public async Task<IActionResult> QuanLyViecLam()
+        {
+            var dsViecLam = await _ViecLamdao.GetAll();
+            return View("~/Views/Admin/QuanLyViecLam.cshtml",dsViecLam);
+        }
+
+
+
+    }
+}
---
+++ QLTuyenDung/Controllers/TuyenDungController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QLTuyenDung.Controllers
+{
+    public class TuyenDungController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
