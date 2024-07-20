05a7636eb0a932b272fa2f75f24919e781f90594
anhduc2204 <101176957+anhduc2204@users.noreply.github.com>
2024-07-21 03:08:26 +0700

    log

--- QLTuyenDung/obj/Debug/net6.0/QLTuyenDung.AssemblyInfo.cs
+++ QLTuyenDung/obj/Debug/net6.0/QLTuyenDung.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QLTuyenDung")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3db43e366834a591b290b4dc5fc7a23452cd00a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ce654426fbe416dd2e1bbc6853dac0d329fb78f")]
 [assembly: System.Reflection.AssemblyProductAttribute("QLTuyenDung")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QLTuyenDung")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
