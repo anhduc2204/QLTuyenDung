185241ebcc17def5f9f5ef63ca69b7c4538e3906
trang2709 <trangchien2709@gmail.com>
2024-07-21 19:39:37 +0700

    init project

---
+++ QLTuyenDung-main/QLTuyenDung/Models/NguoiDung.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.Json.Serialization;
+
+namespace QLTuyenDung.Models
+{
+    [Table("tbl_NguoiDung")]
+    public class NguoiDung
+    {
+      
+       [ Column("iMaND"), DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+       
+        public int MaND { get; set; }
+
+       [Column("sTenND"), Required, StringLength(50)]
+        public string TenND { set; get; }
+
+        [Column("sEmail"), Required, StringLength(50)]
+        public string Email {set; get;}
+
+        [Column("sSDT"), StringLength(15)]
+        public string? SDT {  set; get; }
+
+       [Column("dNgaySinh")]
+        public DateTime? NgaySinh { set; get; }
+
+       [Column("sGioiTinh"), StringLength(10)]
+        public string? GioiTinh {  set; get; }
+        [JsonIgnore]
+        [ForeignKey("iMaTaiKhoan"), Required]
+        
+        public int iMaTaiKhoan { get; set; }
+
+      
+      //  public TaiKhoan TaiKhoan { get; set; }
+
+
+
+    }
+}
